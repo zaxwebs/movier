85266ab6235249bd88ef1373572e0de2b8dd3e2a
zaxwebs <zifidigital@gmail.com>
2023-04-26 16:00:08 +0530

    added inputs

---
+++ src/lib/components/Checkbox.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let id
+	export let name
+	export let value
+</script>
+
+<input type="checkbox" {id} {name} {value} />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+	import Button from '../lib/components/Button.svelte'
+	import Checkbox from '../lib/components/Checkbox.svelte'
+
+	const types = ['Movie', 'TV Show']
+	let selectedTypes = ['Movie']
+
+	const categories = [
+		'Action',
+		'Comedy',
+		'Drama',
+		'Horror',
+		'Romance',
+		'Adventure',
+		'Sci-Fi',
+		'Thriller',
+		'Crime',
+		'Animation',
+		'Fantasy',
+		'Mystery',
+		'Documentary',
+		'Family',
+		'War',
+		'Musical',
+		'Western',
+		'History',
+		'Sport',
+		'Biography',
+	]
+	let selectedCatagories = []
+</script>
+
+<div>
+	<div>
+		<div>Types</div>
+		<div>
+			{#each types as type}
+				<label>
+					<input type="checkbox" bind:group={selectedTypes} value={type} />
+					{type}
+				</label>
+			{/each}
+		</div>
+	</div>
+	<div>
+		<div>Genres</div>
+		<div>
+			{#each categories as category}
+				<label>
+					<input type="checkbox" bind:group={selectedCatagories} value={category} />
+					{category}
+				</label>
+			{/each}
+		</div>
+	</div>
+	<div>
+		<div>Any specifications?</div>
+		<div>
+			<textarea />
+		</div>
+	</div>
+	<Button>Get Started</Button>
+</div>
