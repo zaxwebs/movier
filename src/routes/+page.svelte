ffbfe11f5b46a81f1b8e35d9c67da9d9aa95531b
zaxwebs <zifidigital@gmail.com>
2023-04-29 14:57:05 +0530

    nothing major

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,36 @@
 <script>
-	import Button from '../lib/components/Button.svelte'
+	import Button from "../lib/components/Button.svelte"
 
-	const types = ['Movie', 'TV Show']
-	let selectedTypes = ['Movie']
+	const types = ["Movie", "TV Show"]
+	let selectedTypes = ["Movie"]
 
 	const categories = [
-		'Action',
-		'Comedy',
-		'Drama',
-		'Horror',
-		'Romance',
-		'Adventure',
-		'Sci-Fi',
-		'Thriller',
-		'Crime',
-		'Animation',
-		'Fantasy',
-		'Mystery',
-		'Documentary',
-		'Family',
-		'War',
-		'Musical',
-		'Western',
-		'History',
-		'Sport',
-		'Biography',
+		"Action",
+		"Comedy",
+		"Drama",
+		"Horror",
+		"Romance",
+		"Adventure",
+		"Sci-Fi",
+		"Thriller",
+		"Crime",
+		"Animation",
+		"Fantasy",
+		"Mystery",
+		"Documentary",
+		"Family",
+		"War",
+		"Musical",
+		"Western",
+		"History",
+		"Sport",
+		"Biography",
 	]
 	let selectedCatagories = []
 
 	const fetchList = async () => {
-		const response = await fetch('api/recommend')
-		const data = await response.text()
-		console.log(data)
+		const response = await fetch("api/recommend")
+		console.log(await response.text())
 	}
 </script>
 
@@ -41,7 +40,11 @@
 		<div>
 			{#each types as type}
 				<label>
-					<input type="checkbox" bind:group={selectedTypes} value={type} />
+					<input
+						type="checkbox"
+						bind:group={selectedTypes}
+						value={type}
+					/>
 					{type}
 				</label>
 			{/each}
@@ -52,7 +55,11 @@
 		<div class="flex flex-wrap gap-4">
 			{#each categories as category}
 				<label>
-					<input type="checkbox" bind:group={selectedCatagories} value={category} />
+					<input
+						type="checkbox"
+						bind:group={selectedCatagories}
+						value={category}
+					/>
 					{category}
 				</label>
 			{/each}
--- src/routes/api/recommend/+server.js
+++ src/routes/api/recommend/+server.js
@@ -1,20 +1,22 @@
-import { SECRET_API_KEY } from '$env/static/private'
-import { Configuration, OpenAIApi } from "openai";
-
+import { SECRET_API_KEY } from "$env/static/private"
+import { Configuration, OpenAIApi } from "openai"
 
 export const GET = async () => {
 	const configuration = new Configuration({
 		apiKey: SECRET_API_KEY,
-	});
-	const openai = new OpenAIApi(configuration);
+	})
+	const openai = new OpenAIApi(configuration)
 
 	const response = await openai.createChatCompletion({
 		model: "gpt-3.5-turbo",
 		messages: [
-			{ role: 'user', content: 'give me a list of top 10 tv shows like friends' }
+			{
+				role: "user",
+				content: 'give me a list of top 10 tv shows like "friends"',
+			},
 		],
-	});
+	})
 
-	console.log(response.data.choices);
-	return new Response(response.data.choices[0].message.content);
+	console.log(response.data.choices)
+	return new Response(response.data.choices[0].message.content)
 }
