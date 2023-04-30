80c3e78912bbc86ce847abf70ecfe0c1597e5bf9
zaxwebs <zifidigital@gmail.com>
2023-04-30 14:36:32 +0530

    added posters

--- src/lib/components/RecommendationCard.svelte
+++ src/lib/components/RecommendationCard.svelte
@@ -16,8 +16,10 @@
 <div class="flex flex-col rounded md:flex-row border">
 	<img
 		class="h-40 w-full rounded-t-lg object-cover md:h-full md:w-48 md:rounded-none md:rounded-l-lg"
-		src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-		alt=""
+		src={details
+			? `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${details.poster_path}`
+			: 'https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg'}
+		alt="poster"
 	/>
 	<div class="flex flex-col justify-start p-6">
 		<h5 class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">{name}</h5>
